c9882ca443d0a66ad93cb9147f5c37b74a67f306
Shimmerlie <1766433262@qq.com>
2026-05-17 14:38:43 +0800

    add learn

---
+++ learn.py
@@ -0,0 +1,27 @@
+import math
+
+a = -1
+b = -2
+c = 3
+print((-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a))
+print((-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a))
+
+s = "hello world"
+print(len(s))
+print(s[len(s) - 1])
+
+n = None
+
+print(type(s))
+print(type(n))
+
+shoppint_list = ["键盘","键帽"]
+shoppint_list.append("显示器")
+
+s = "Hello"
+print(s.upper())
+
+total = 0
+for i in range(1,101):
+    total = total + i
+print(total)
